46b434669fc9d750aa6e6b49a5ac5cfc64889a5d
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-09 01:18:05 -0300

    create custom mui card component

---
+++ src/components/mui/Card.tsx
@@ -0,0 +1,7 @@
+import { Card, CardProps } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const CustomCard = styled(Card)<CardProps>(() => ({
+  borderRadius: 8,
+  padding: 32,
+}));
