f079edb4d5a0e0da34cac4e75276570fdc66659e
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-10 02:03:14 -0300

    feat: add chip mui client component

--- src/components/mui/Container.tsx
+++ src/components/mui/Container.tsx
@@ -3,8 +3,4 @@
 import { Container, ContainerProps } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const CustomContainer = styled(Container)<ContainerProps>(() => ({
-  borderRadius: 8,
-  padding: 32,
-  marginBottom: 24,
-}));
+export const CustomContainer = styled(Container)<ContainerProps>(() => ({}));
