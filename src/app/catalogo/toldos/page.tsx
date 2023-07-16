54fc5f4d31edeb1b16f318158694bf10c2dab5c1
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-15 23:36:26 -0300

    style: responsive update for page

--- src/app/catalogo/toldos/page.tsx
+++ src/app/catalogo/toldos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { CatalogItem } from "@/components/CatalogItem";
+import { CatalogItem } from "@/components/catalog/CatalogItem";
 import { Heading } from "@/components/Heading";
 import { CustomCard } from "@/components/mui/Card";
 import { Divider } from "@mui/material";
@@ -13,54 +13,61 @@ import colourable_2 from "@/assets/rares/marquee/rare_colourable_marquee_2-64-2-
 import yellow from "@/assets/rares/marquee/yellow_marquee.png";
 
 export default function Toldos() {
+  const catalogItems = [
+    {
+      image: green.src,
+      price: 30,
+      title: "Verde limão",
+      furniName: "green marquee",
+    },
+    {
+      image: maroon.src,
+      price: 30,
+      title: "Maroon",
+      furniName: "maroon marquee",
+    },
+    {
+      image: colourable_1.src,
+      price: 30,
+      title: "Safira",
+      furniName: "rare_colourable_marquee_1_name",
+    },
+    {
+      image: colourable_2.src,
+      price: 60,
+      title: "Esmeralda",
+      furniName: "rare_colourable_marquee_2_name",
+    },
+    {
+      image: yellow.src,
+      price: 30,
+      title: "Amarelo",
+      furniName: "yellow marquee",
+    },
+  ];
+
   return (
     <CustomCard variant="outlined">
       <Heading>Toldos</Heading>
 
       <Divider sx={{ marginTop: 2, marginBottom: 4 }} />
 
-      <div className="grid grid-cols-3 gap-4">
-        <CatalogItem
-          image={green.src}
-          price={50}
-          title="Verde limão"
-          furniName="green marquee"
-        />
-
-        <CatalogItem
-          image={maroon.src}
-          price={50}
-          title="Maroon"
-          furniName="maroon marquee"
-        />
-
-        <CatalogItem
-          image={purple.src}
-          price={200}
-          title="Pitaya rosa"
-          furniName="purple marquee"
-        />
-
-        <CatalogItem
-          image={colourable_1.src}
-          price={50}
-          title="Safira"
-          furniName="rare_colourable_marquee_1_name"
-        />
-
-        <CatalogItem
-          image={colourable_2.src}
-          price={70}
-          title="Esmeralda"
-          furniName="rare_colourable_marquee_2_name"
-        />
-
-        <CatalogItem
-          image={yellow.src}
-          price={50}
-          title="Amarelo"
-          furniName="yellow marquee"
-        />
+      <div
+        className="
+      grid grid-cols-3 gap-4
+      max-sm:grid-cols-2
+      max-[384px]:grid-cols-1
+      "
+      >
+        {catalogItems.map((item) => (
+          <CatalogItem
+            image={item.image}
+            price={item.price}
+            title={item.title}
+            furniName={item.furniName}
+            key={item.title}
+          />
+        ))}
       </div>
     </CustomCard>
   );
