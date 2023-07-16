7738fff81965bb2c875cf4c057aa67c5b05f8d50
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-15 21:35:38 -0300

    chore: update prices at pricing table

--- src/app/pagamento/page.tsx
+++ src/app/pagamento/page.tsx
@@ -16,38 +16,34 @@ import {
 export default function Pagamento() {
   function createData(
     service: string,
-    minTeam: number,
-    maxTeam: number,
-    minGea: number,
-    maxGea: number
+    team: number | string,
+    gea: number | string
   ) {
     return {
       service,
-      minTeam,
-      maxTeam,
-      minGea,
-      maxGea,
+      team,
+      gea,
     };
   }
 
   const paymentTableRows = [
-    // serviço - minimo equipe - max equipe - minimo gea - max gea
-    createData("Construção pequena", 0, 10, 0, 10),
-    createData("Construção média", 0, 15, 0, 40),
-    createData("Construção grande", 0, 30, 0, 60),
-    createData("Programação pequena", 0, 10, 0, 10),
-    createData("Programação média", 0, 15, 0, 40),
-    createData("Programação grande", 0, 30, 0, 60),
-    createData("Logo", 0, 30, 0, 50),
-    createData("Expositor", 0, 20, 0, 40),
-    createData("Emblema", 0, 15, 0, 30),
-    createData("Emblema de ranking", 0, 10, 0, 20),
-    createData("Ads de quarto", 0, 40, 0, 60),
-    createData("Ads de click", 0, 15, 0, 30),
-    createData("Edição de ads de quarto", 0, 15, 0, 25),
-    createData("Edição de ads de click", 0, 10, 0, 15),
-    createData("Grosser", 0, 20, 0, 40),
-    createData("Flyers", 0, 25, 0, 35),
+    // serviço - preço equipe - preço gea
+    createData("Construção pequena", 40, 80),
+    createData("Construção média", 80, 120),
+    createData("Construção grande", 120, 200),
+    createData("Programação pequena", 40, 80),
+    createData("Programação média", 60, 120),
+    createData("Programação grande", 120, 200),
+    createData("Logo", 120, 200),
+    createData("Expositor", 80, 140),
+    createData("Emblema", 80, 120),
+    createData("Emblema de ranking", "40 (+10/emblema)", 80),
+    createData("Ads de quarto", 160, 240),
+    createData("Ads de click", 60, 120),
+    createData("Edição de ads de quarto", 60, 100),
+    createData("Edição de ads de click", 30, 60),
+    createData("Grosser", 80, 140),
+    createData("Flyers", 100, 140),
   ];
 
   return (
@@ -62,10 +58,8 @@ export default function Pagamento() {
             <TableHead>
               <TableRow>
                 <TableCell>Serviço</TableCell>
-                <TableCell>Menor valor (equipe)</TableCell>
-                <TableCell>Maior valor (equipe)</TableCell>
-                <TableCell>Menor valor (gea)</TableCell>
-                <TableCell>Maior valor (gea)</TableCell>
+                <TableCell>Valor Equipe</TableCell>
+                <TableCell>Valor GeA</TableCell>
               </TableRow>
             </TableHead>
 
@@ -78,19 +72,11 @@ export default function Pagamento() {
                     </TableCell>
 
                     <TableCell component="th" scope="row">
-                      {row.minTeam}
+                      {row.team}
                     </TableCell>
 
                     <TableCell component="th" scope="row">
-                      {row.maxTeam}
-                    </TableCell>
-
-                    <TableCell component="th" scope="row">
-                      {row.minGea}
-                    </TableCell>
-
-                    <TableCell component="th" scope="row">
-                      {row.maxGea}
+                      {row.gea}
                     </TableCell>
                   </TableRow>
                 );
